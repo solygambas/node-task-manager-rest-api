b67049b5fe7bc8a56043406e230c8918b1f68702
solygambas <solygambas@gmail.com>
2020-05-02 13:22:32 +0200

    add yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -8,12 +8,76 @@
 // console.log(sum);
 
 // const validator = require("validator");
-const chalk = require("chalk");
-const getNotes = require("./notes");
-
-const notes = getNotes();
-console.log(notes);
-console.log(chalk.green.bold.inverse("Success!"));
-
 // console.log(validator.isEmail("andrew@example.com"));
 // console.log(validator.isURL("https://mead.io"));
+
+const chalk = require("chalk");
+const yargs = require("yargs");
+const getNotes = require("./notes");
+
+// const notes = getNotes();
+// console.log(notes);
+// console.log(chalk.green.bold.inverse("Success!"));
+
+// console.log(process.argv)
+
+// const command = process.argv[2];
+
+// if (command === "add") {
+//   console.log("adding note!");
+// } else if (command === "remove") {
+//   console.log("removing note!");
+// }
+
+yargs.version("1.1.0");
+
+// add
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log(`Title: ${argv.title} \nBody: ${argv.body}`);
+  },
+});
+
+// remove
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("removing note!");
+  },
+});
+
+// list
+yargs.command({
+  command: "list",
+  describe: "List all notes",
+  handler: function () {
+    console.log("listing out all notes!");
+  },
+});
+
+// read
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("reading note!");
+  },
+});
+
+// console.log(yargs.argv);
+yargs.parse();
