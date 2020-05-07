9d4b50b906f3427986e96d9e13695d0743bf0253
solygambas <solygambas@gmail.com>
2020-05-07 08:33:50 +0200

    add create and read routes

---
+++ task-manager/src/models/task.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// const task = new Task({
+//   description: "Buy apples  ",
+//   completed: true,
+// });
+
+// task
+//   .save()
+//   .then(() => console.log(task))
+//   .catch((error) => console.log("Error!", error));
+
+module.exports = Task;
---
+++ task-manager/src/models/user.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Choose a more complex password");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+});
+
+// const user = new User({
+//   name: "   John ",
+//   email: "Mike@gmAIL.com   ",
+//   password: "PASSWORD123 ",
+// });
+
+// user
+//   .save()
+//   .then(() => console.log(user))
+//   .catch((error) => console.log("Error!", error));
+
+module.exports = User;
