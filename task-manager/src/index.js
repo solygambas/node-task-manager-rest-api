590700e1dc4a8f6766038efee8b225a61e26785d
solygambas <solygambas@gmail.com>
2020-05-07 14:02:47 +0200

    add bcrypt and middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -17,3 +17,14 @@ app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(port, () => console.log(`Server is up at http://localhost:${port}`));
+
+// const bcrypt = require("bcryptjs");
+
+// const myFunction = async () => {
+//   const password = "redisred";
+//   const hashedPassword = await bcrypt.hash(password, 8);
+//   console.log(hashedPassword);
+//   const isMatch = await bcrypt.compare(password, hashedPassword);
+//   console.log(isMatch);
+// };
+// myFunction();
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const User = mongoose.model("User", {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -39,6 +40,17 @@ const User = mongoose.model("User", {
     },
   },
 });
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  // console.log("just before saving");
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
 
 // const user = new User({
 //   name: "   John ",
