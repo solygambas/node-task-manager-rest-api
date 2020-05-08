b8d8f3227fc3f8a3b211ace2bc17325e053522c0
solygambas <solygambas@gmail.com>
2020-05-08 14:27:25 +0200

    add sorting, filtering and pagination

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -23,12 +23,31 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+  }
   try {
-    const tasks = await Task.find({ owner: req.user._id });
-    res.send(tasks);
+    // const tasks = await Task.find({ owner: req.user._id });
+    // res.send(tasks);
     // alternative:
-    // await req.user.populate("tasks").execPopulate();
-    // res.send(req.user.tasks);
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
+      })
+      .execPopulate();
+    res.send(req.user.tasks);
   } catch (error) {
     res.status(500).send();
   }
