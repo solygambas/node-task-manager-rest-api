856aa529715fda4db55c026ed0702b9f15d04e42
solygambas <solygambas@gmail.com>
2020-05-08 10:27:12 +0200

    add middleware, routers and jwt

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,17 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("GET requests are disabled");
+//   } else {
+//     next();
+//   }
+// });
+// app.use((req, res, next) => {
+//   res.status(503).send("Maintenance. Come back later.");
+// });
+
 app.use(express.json());
 
 // const router = new express.Router();
@@ -28,3 +39,23 @@ app.listen(port, () => console.log(`Server is up at http://localhost:${port}`));
 //   console.log(isMatch);
 // };
 // myFunction();
+
+// const jwt = require("jsonwebtoken");
+// const myFunction = async () => {
+//   const token = jwt.sign({ _id: "aaasbsd" }, "thismynodecourse", {
+//     expiresIn: "7 days",
+//   });
+//   console.log(token);
+//   const data = jwt.verify(token, "thismynodecourse");
+//   console.log(data);
+// };
+// myFunction();
+
+const pet = { name: "Hal" };
+
+pet.toJSON = function () {
+  console.log(this);
+  return {};
+};
+
+console.log(JSON.stringify(pet));
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = new express.Router();
+const auth = require("../middleware/auth");
 const User = require("../models/user");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -16,16 +18,46 @@ router.post("/users", async (req, res) => {
   //     .catch((error) => res.status(400).send(error));
 });
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send();
-  }
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
   //   User.find({})
   //     .then((users) => res.send(users))
   //     .catch((error) => res.status(500).send());
+});
+
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 router.get("/users/:id", async (req, res) => {
